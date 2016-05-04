ecc4ad12695738b6cc38aea5b93982ad5d9b1e08
hujunjian <hujunjian@gu360.com>
2016-05-04 19:37:05 +0800

    添加点击事件

--- SRStockKeyboard/SRStockSysKeyboard.swift
+++ SRStockKeyboard/SRStockSysKeyboard.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
+enum KeyPopType {
+    case Normal
+    case Left
+    case Right
+}
+
 let kCharKeys = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
 
 class SRStockSysKeyboard: UIView {
     
     var  delegate : SRStockKeyboardDelegate?
+    
+    var keyPop : UIImageView?
     
     @IBOutlet var charButtons: [SRStockSysKeyButton]!
     
@@ -68,5 +77,111 @@ class SRStockSysKeyboard: UIView {
             delegate?.searchDidPressed()
         }
     }
+   
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        super.touchesBegan(touches, withEvent: event)
+        self.touchOnButton(touches)
+    }
     
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        super.touchesMoved(touches, withEvent: event)
+        self.touchOnButton(touches)
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        super.touchesEnded(touches, withEvent: event)
+    }
+    
+    override func touchesCancelled(touches: Set<UITouch>?, withEvent event: UIEvent?) {
+        super.touchesCancelled(touches, withEvent: event)
+    }
+    
+    
+    func touchOnButton(touches: Set<UITouch>) {
+        let touch = touches.first
+        let point = touch?.locationInView(self)
+        var button: UIButton?
+        for tmpButton in charButtons {
+            if tmpButton.frame.contains(point!) {
+                button = tmpButton
+                break
+            }
+        }
+        if button == nil {
+            return
+        }
+        self.addPopupToButton(button)
+    }
+    
+    func addPopupToButton(button : UIButton?) {
+        if keyPop?.tag == button?.tag {
+            return
+        }
+        keyPop?.removeFromSuperview()
+        keyPop = nil
+        
+        let textLabel = UILabel()
+        
+        if button?.tag == 0 {
+            keyPop = self.createKeyPopImageWithType(KeyPopType.Right, withRect:button?.frame)
+        }else if button?.tag == 9 {
+            keyPop = self.createKeyPopImageWithType(KeyPopType.Left, withRect:button?.frame)
+        }else {
+            keyPop = self.createKeyPopImageWithType(KeyPopType.Normal, withRect:button?.frame)
+        }
+        
+        textLabel.text = kCharKeys[(button?.tag)!]
+        textLabel.textAlignment = .Center
+        textLabel.backgroundColor = UIColor.clearColor()
+        textLabel.font = UIFont.systemFontOfSize(44)
+        
+        keyPop?.layer.shadowColor = UIColor.whiteColor().colorWithAlphaComponent(1.0).CGColor
+        keyPop?.layer.shadowOffset = CGSizeMake(0, 3.0)
+        keyPop?.layer.shadowOpacity = 1
+        keyPop?.layer.shadowRadius = 5.0
+        keyPop?.clipsToBounds = true
+        
+        keyPop?.addSubview(textLabel)
+        keyPop?.tag = 1
+        button?.addSubview(keyPop!)
+    }
+    
+    func createKeyPopImageWithType(type:KeyPopType, withRect rect:CGRect?) -> UIImageView {
+        
+        let path = CGPathCreateMutable()
+        var point = CGPointZero
+       
+        let radius: CGFloat = 5.0
+        let topWith : CGFloat = (rect?.size.width)! * 1.3
+        let upperHeight : CGFloat = 56.0
+        let lowerHeight : CGFloat = 37.0
+        
+        point.x += radius
+        CGPathMoveToPoint(path, nil, point.x, point.y)
+        point.x += topWith
+        CGPathAddLineToPoint(path, nil, point.x, point.y)
+        point.y += radius
+        CGPathAddArcToPoint(path, nil, point.x, point.y, point.x + radius, point.y, CGFloat(M_PI_4))
+        point.x += radius
+        point.y += upperHeight
+        CGPathAddLineToPoint(path, nil, point.x, point.y)
+        point.x += topWith
+        CGPathAddLineToPoint(path, nil, point.x, point.y)
+        point.x += topWith
+        CGPathAddLineToPoint(path, nil, point.x, point.y)
+        
+        point.x = radius
+        point.y = 0
+        CGPathAddLineToPoint(path, nil, point.x, point.y)
+        
+        let imageSize = CGSizeMake(topWith + radius * 2, upperHeight + radius * 2 + lowerHeight)
+        
+        UIGraphicsBeginImageContext(imageSize)
+        let context = UIGraphicsGetCurrentContext()
+        CGContextAddPath(context, path)
+        let imageRef = CGBitmapContextCreateImage(context)
+        let imageView = UIImageView(image: UIImage(CGImage: imageRef!))
+        imageView.frame = CGRectMake(((rect?.size.width)! - imageSize.width) / 2, (rect?.height)! - imageSize.height, imageSize.width, imageSize.height)
+        return imageView
+    }
 }
